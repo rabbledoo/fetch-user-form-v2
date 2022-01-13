9d98ed299ec07bd70c5e807983368e4cd97e2307
rabbledoo <rabbledoo@github.com>
2022-01-12 20:32:08 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import useForm from "react-hook-form";
 
 function App() {
+  const {register, handleSubmit} = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" placeholder="Name" name="name" ref={register} />
+          <input type="email" placeholder="Email" name="email" ref={register} />
+          <input type="password" placeholder="Password" ref={register} />
+          <button type="submit" ></button>
+        </form>
+      </div>
+    </>
   );
 }
 
